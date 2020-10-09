3332d5c9ce2e77439d13c6ab65efcdee3f389d08
Jakub <jakub.jakubecky123@gmail.com>
2020-10-09 18:21:17 +0200

    c

--- main.cpp
+++ main.cpp
@@ -113,10 +113,11 @@ int main() {
     getline(cin, line);
     vector<string> args = split(line, ' ');
     int wallsCount = stoi(args[0].c_str());
-    int end = stoi(args[1]);
-    int jumps = 0;
+    int end = stoi((args[1].c_str()));
     Vector2I startPos = Vector2I(0,0);
     Domain lastDomain = Domain(startPos.getY(), startPos.getY(), startPos.getX());//should be last domain
+    if(wallsCount == 0) {cout<<0;return 0;}
+
     for (int i = 0; i < wallsCount; i++) {
         getline(cin, line);
         vector<string> args = split(line, ' ');
@@ -133,22 +134,31 @@ int main() {
         if(!isOKConnection(lastDomain.getMaxVector(), Vector2I(x, maxY))){
             maxY-=1;
         }
-
+        if(minY >= up || maxY <= down){
+            cout<<"NIE";
+            return 0;
+        }
         int minYFlight = -x + lastDomain.getMax()+lastDomain.getWallX();
 
         int maxYFlight = x + lastDomain.getMin()-lastDomain.getWallX();
 
         Collection coll1 = Collection(minYFlight, maxYFlight);
         Collection coll2 = Collection(minY, maxY);
+
         Collection result = coll1.commonPart(coll2);
+        if(end < x){
+            cout.precision(10);
+            cout<<getPeak(startPos, Vector2I(lastDomain.getMinVector()));
+            return 0;
+        }
         if(result.isEmpty()){
             cout<<"NIE";
             return 0;
         }
         lastDomain = Domain(result.getMin(), result.getMax(), x);
         if(i + 1 == wallsCount){
+            cout.precision(10);
             cout<<getPeak(startPos, lastDomain.getMinVector());
-            cout<<"\nend";
         }
     }
     return 0;
